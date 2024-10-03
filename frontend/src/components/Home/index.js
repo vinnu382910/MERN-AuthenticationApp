f718b4acb579a5c63e8cf6a755ce00a4983d205f
Kalva Vinay <80328622+vinnu382910@users.noreply.github.com>
2024-10-03 20:58:05 +0530

    Update index.js

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -23,7 +23,7 @@ const Home = () => {
 
     const fetchProducts = async() => {
         try{
-            const url = 'http://localhost:8080/products'
+            const url = 'https://mern-authentication-app-api.vercel.app/products'
             const headers = {
                 headers: {'Authorization': localStorage.getItem('token')}
             }
