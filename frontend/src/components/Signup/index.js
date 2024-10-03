80cd9a39b2e924873f4c6894eaa8954b86e60b3c
Kalva Vinay <80328622+vinnu382910@users.noreply.github.com>
2024-10-03 20:56:56 +0530

    Update index.js

--- frontend/src/components/Signup/index.js
+++ frontend/src/components/Signup/index.js
@@ -27,7 +27,7 @@ const Signup = () => {
             handleError('name, email and password are required!')
         }
         try {
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://mern-authentication-app-api.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
